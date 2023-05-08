6fc06eaeaa43cfefafd17573a5d50ce0a556afd5
Luigi Lorentz Lolata <104982562+Treinck@users.noreply.github.com>
2023-05-08 12:53:04 -0400

    Update Jogador.java

--- Projeto  - Teste/src/principal/Jogador.java
+++ Projeto  - Teste/src/principal/Jogador.java
@@ -4,6 +4,7 @@ public class Jogador {
 	private String name;
 	private int hp = 30;
 	private int dmg = 3;
+	public boolean shield = false;
 	
 	public Jogador(){
 	}
@@ -22,6 +23,9 @@ public class Jogador {
 	}
 	public void setDMG(int new_dmg) {
 		dmg = new_dmg;
+	}
+	public void setShield(boolean new_shield) {
+		shield = new_shield;
 	}
 }
 //System.out.printf("");
